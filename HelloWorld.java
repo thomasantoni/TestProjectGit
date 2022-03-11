f19ccaac8cf8efe8b21cc5c559f9187e246ffc70
patrick.lapsien <patrick.lapsien@accenture.com>
2022-03-11 08:40:56 +0100

    new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
     public static void main(String [] args){
         System.out.println("Hello World ");
-<<<<<<< HEAD
 	System.out.println("Hello World ");
 	System.out.println("Hello World ");
-=======
         System.out.println("Hello World ");
->>>>>>> 17dea5785a2c38d203ef14885f71006280f4e836
     }
 }
